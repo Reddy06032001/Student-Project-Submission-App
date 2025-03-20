4189817174c8fc2b48ba0ba664f54f27d1e21186
Reddy06032001 <ashokredd2001@gmail.com>
2025-03-21 02:36:04 +0530

    Fix MongoDB connection options

--- config/db.js
+++ config/db.js
@@ -2,16 +2,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB Connected');
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI); // No need for options
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
