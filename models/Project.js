dae58458bee9be8bfb09bc9e18c43d94755555ee
Reddy06032001 <ashokredd2001@gmail.com>
2025-03-21 22:35:02 +0530

    database schema change

--- models/Project.js
+++ models/Project.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const ProjectSchema = new mongoose.Schema({
   studentNames: [String], // Array of 4 student names
+  studentRollNo: [String],
   projectName: String,
   year: String,
   semester: String,
--- routes/projects.js
+++ routes/projects.js
@@ -1,18 +1,39 @@
-const express = require('express');
-const Project = require('../models/Project');
+const express = require("express");
+const Project = require("../models/Project");
 
 const router = express.Router();
 
 // Create Project
-router.post('/submit', async (req, res) => {
+router.post("/submit", async (req, res) => {
   try {
-    const { studentNames, projectName, year, semester, section } = req.body;
+    const {
+      studentNames,
+      studentRollNo,
+      projectName,
+      year,
+      semester,
+      section,
+    } = req.body;
 
     if (studentNames.length !== 4) {
-      return res.status(400).json({ message: "Exactly 4 student names are required." });
+      return res
+        .status(400)
+        .json({ message: "Exactly 4 student names are required." });
+    }
+    if (studentRollNo.length !== 4) {
+      return res
+        .status(400)
+        .json({ message: "Exactly 4 student Roll Numbers are required." });
     }
 
-    const newProject = new Project({ studentNames, projectName, year, semester, section });
+    const newProject = new Project({
+      studentNames,
+      studentRollNo,
+      projectName,
+      year,
+      semester,
+      section,
+    });
     await newProject.save();
 
     res.status(201).json({ message: "Project Submitted Successfully!" });
@@ -22,7 +43,7 @@ router.post('/submit', async (req, res) => {
 });
 
 // Get All Projects
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const projects = await Project.find();
     res.status(200).json(projects);
